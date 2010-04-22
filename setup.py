5d1b402919d7496b1d245f3c9df277af57385148
Glen Zangirolami <glenbot@gmail.com>
2010-04-22 13:34:32 -0500

    Added setup.py script.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(author='Glen Zangirolami',
+      author_email='glenbot@gmail.com',
+      description='PyParseURI - allows you to parse URIs easily',
+      long_description='PyParseURI - allows you to parse URIs easily. Original code was ' \
+                       'written in JavaScript by Steven Levithan',
+      fullname='pyparseuri',
+      name='pyparseuri',
+      py_modules=['parse_uri'],
+      url='http://github.com/glenbot/pyparseuri',
+      download_url='http://github.com/glenbot/pyparseuri',
+      version='0.1',
+      license='GPL',
+      platforms=['Linux','Windows']
+)
